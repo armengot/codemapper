f1489bb7c0b41a71f24d540d9289457573a9efa7
Marcelo Armengot <marcelo.armengot@gmail.com>
2024-03-09 23:07:02 +0100

    GUI future project.

---
+++ gui/main.cpp
@@ -0,0 +1,8 @@
+
+// Menu file
+//      -> parse input -> svg
+//      -> save graph
+//      -> export (render)
+//      -> quit
+// Menu help
+//      -> about
--- lib/gitinfo.h
+++ lib/gitinfo.h
@@ -3,8 +3,8 @@
 #define GITINFO_H
 #define GITAUTHORID "marcelo.armengot@gmail.com"
 #define GIT_MACHINE "omen"
-#define GIT_DTETIME "2024-03-09-22:27:46"
-#define GIT_LNGHASH "c60713e56b5268f12a9cb66089ef1c9cfbf65d0e"
+#define GIT_DTETIME "2024-03-09-22:52:38"
+#define GIT_LNGHASH "4cf92ec605b8092e276241b420e8db74b73b9424"
 #define GIT_USERCXX "marcelo.armengot@gmail.com"
-#define GIT_OFFICIAL_VERSION "REL_00.01"
+#define GIT_OFFICIAL_VERSION "REL_00.03"
 #endif
