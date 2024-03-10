24b1ab3bc056e7f7ec6d3e6b1f9680ea5461b39e
Marcelo Armengot <marcelo.armengot@gmail.com>
2024-03-10 19:40:09 +0100

    Branches merging ensurement.

--- lib/gitinfo.h
+++ lib/gitinfo.h
@@ -3,8 +3,8 @@
 #define GITINFO_H
 #define GITAUTHORID "marcelo.armengot@gmail.com"
 #define GIT_MACHINE "omen"
-#define GIT_DTETIME "2024-03-10-11:03:07"
-#define GIT_LNGHASH "a08a3520e46557731f71992eabe0a36f432bc357"
+#define GIT_DTETIME "2024-03-10-19:38:48"
+#define GIT_LNGHASH "701c13a79cd2898d848df9d53c2ed887acb77a7c"
 #define GIT_USERCXX "marcelo.armengot@gmail.com"
-#define GIT_OFFICIAL_VERSION "DEV_00.03.02"
+#define GIT_OFFICIAL_VERSION "REL_00.04.01"
 #endif
