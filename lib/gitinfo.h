d3054cc69cbd84c99d3ae4ab461d30273c2ae53f
Marcelo Armengot <marcelo.armengot@gmail.com>
2024-03-13 21:45:33 +0100

    version info

--- lib/gitinfo.h
+++ lib/gitinfo.h
@@ -3,8 +3,8 @@
 #define GITINFO_H
 #define GITAUTHORID "marcelo.armengot@gmail.com"
 #define GIT_MACHINE "omen"
-#define GIT_DTETIME "2024-03-12-22:15:41"
-#define GIT_LNGHASH "b67dbb723ef5b28367fc466f5840aadeb6d6f702"
+#define GIT_DTETIME "2024-03-13-21:45:03"
+#define GIT_LNGHASH "b1c79666672f59db87b0ed5b9845116086f40446"
 #define GIT_USERCXX "marcelo.armengot@gmail.com"
-#define GIT_OFFICIAL_VERSION "DEV_00.03.04"
+#define GIT_OFFICIAL_VERSION "DEV_00_03.05"
 #endif
