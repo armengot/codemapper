735516eba756dbe536737a880235539fd05a7fe0
Marcelo Armengot <marcelo.armengot@gmail.com>
2024-03-24 17:51:30 +0100

    Improved REL.

--- lib/gitinfo.h
+++ lib/gitinfo.h
@@ -3,10 +3,10 @@
 #define GITINFO_H
 #define GITAUTHORID "marcelo.armengot@gmail.com"
 #define GIT_MACHINE "omen"
-#define GIT_DTETIME "2024-03-24-17:32:22"
-#define GIT_LNGHASH "ba787337f13eb402b651388778d2de6208e8a4c9"
+#define GIT_DTETIME "2024-03-24-17:50:09"
+#define GIT_LNGHASH "3f27c1ec1950b0a4438dc617f15e7fbc11673d7f"
 #define GIT_USERCXX "marcelo.armengot@gmail.com"
-#define GIT_OFFICIAL_VERSION "DEV_00_04.02"
+#define GIT_OFFICIAL_VERSION "DEV_00_04.03"
 #define CODEMAPPER_AUTHOR "Marcelo Armengot"
 #define CODEMAPPER_URL "https://github.com/armengot/codemapper"
 #endif
