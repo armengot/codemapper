b9ce32d541aed5a7f33d58916532f8f4210ff29b
Marcelo Armengot <marcelo.armengot@gmail.com>
2024-04-02 21:32:47 +0200

    CMake checks.

--- lib/gitinfo.h
+++ lib/gitinfo.h
@@ -1,12 +1,12 @@
 // Get GIT info
 #ifndef GITINFO_H
 #define GITINFO_H
-#define GITAUTHORID "marmengot@indra.es"
-#define GIT_MACHINE "11LAP5CG3155W9F-linux"
-#define GIT_DTETIME "2024-03-31-23:44:36"
-#define GIT_LNGHASH "5fa56657f7c26bb65a95d09fa062b90996df454f"
-#define GIT_USERCXX "5396496-armengot@users.noreply.gitlab.com"
-#define GIT_OFFICIAL_VERSION "DEV_00_06.02"
+#define GITAUTHORID "marcelo.armengot@gmail.com"
+#define GIT_MACHINE "omen"
+#define GIT_DTETIME "2024-04-02-21:31:45"
+#define GIT_LNGHASH "e982938043ff8e90aed435f6587cc8e0326c25dc"
+#define GIT_USERCXX "marcelo.armengot@gmail.com"
+#define GIT_OFFICIAL_VERSION "DEV_00_06.03"
 #define CODEMAPPER_AUTHOR "Marcelo Armengot"
 #define CODEMAPPER_URL "https://github.com/armengot/codemapper"
 #define SYSTEM_DETECTED "LINUX"
